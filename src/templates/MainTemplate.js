9a6147edebc81c87a260d03ce069423e7bdfe9de
Karol Barzowski <gamekarol@gmail.com>
2020-09-09 21:39:31 +0200

    Notification sync

--- src/templates/MainTemplate.js
+++ src/templates/MainTemplate.js
@@ -6,14 +6,25 @@ import { theme } from 'theme/mainTheme';
 
 function MainTemplate({ children }) {
   const [isUpdate] = useState(JSON.parse(window.localStorage.getItem('isUpdate')));
+  const [isNotSynced] = useState(JSON.parse(window.localStorage.getItem('isNotSynced')));
   const { enqueueSnackbar } = useSnackbar();
 
   useEffect(() => {
     if (isUpdate) {
-      enqueueSnackbar('Zaktualizowano dane', { variant: 'success', autoHideDuration: 3000 });
+      enqueueSnackbar('Zaktualizowano dane', { variant: 'success', autoHideDuration: 5000 });
       window.localStorage.removeItem('isUpdate');
     }
   }, [isUpdate, enqueueSnackbar]);
+
+  useEffect(() => {
+    if (isNotSynced) {
+      enqueueSnackbar('Brak nowych danych do synchronizacji', {
+        variant: 'info',
+        autoHideDuration: 5000,
+      });
+      window.localStorage.removeItem('isNotSynced');
+    }
+  }, [isNotSynced, enqueueSnackbar]);
 
   return <ThemeProvider theme={theme}>{children}</ThemeProvider>;
 }
